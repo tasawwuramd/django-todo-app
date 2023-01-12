6c51aa3156873011811250e83dd6c80f3c8ed478
Tasawwur Ahmad -X (tasahmad - TATA CONSULTANCY SERVICES LIMITED at Cisco) <tasahmad@cisco.com>
2023-01-12 11:47:21 +0530

    login and logout completed v66

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 
 urlpatterns=[
-    path('signup/',views.signupuser),
+    path('signup/',views.signupuser , name='signupuser'),
     path('current/',views.currenttodos , name='currenttodos'),
+    path('',views.homepage , name='homepage'),
+
+    path('login/',views.loginuser , name='loginuser'),
+    path('logout/',views.logoutuser , name='logoutuser'),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,8 @@
 from django.db import IntegrityError
 from django.shortcuts import render , redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login , logout , authenticate
 
 # Create your views here.
 
@@ -23,6 +23,24 @@ def signupuser(request):
         else:
             return render(request, 'todo/signup.html' , {'form': UserCreationForm() , 'error' : 'Password didn`t match'})
 
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/login.html' , {'form': AuthenticationForm() })
+    elif request.method == 'POST':
+        user = authenticate(request, username=request.POST['username'],password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/login.html' , {'form': AuthenticationForm()  , 'error' : 'Username and password didn`t match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
+
+def homepage(request):
+    return render(request, 'todo/home.html')
+
+def logoutuser(req):
+    if req.method == 'POST':
+        logout(req)
+        return redirect('homepage')
